2e3393efd31b902cbd8c01ebaf11fa68395c56a0
Rodrigo Cercado Lescano <rodrigo.cercado@pucp.edu.pe>
2025-10-07 11:05:50 -0500

    Agregado de capa Controller

--- CONTROLLER/CONTROLLER.h
+++ CONTROLLER/CONTROLLER.h
@@ -1,10 +1,33 @@
 #pragma once
 
-using namespace System;
+namespace BotController {
+	using namespace System::Collections::Generic;
+	using namespace BotModel;
+	using namespace System;
 
-namespace CONTROLLER {
-	public ref class Class1
-	{
-		// TODO: Agregue aquí los métodos de esta clase.
+	public ref class CONTROLLER {
+	private:
+		List<DatosUsuario^>^ listaUsuarios;
+		List<Robot^>^ listaRobots;
+	public:
+		CONTROLLER();
+		void registrarUsuario(int id, String^ nombre, String^ contra, String^ cargo);
+		void registrarUsuarioAutoID(String^ nombre, String^ contra, String^ cargo);
+		DatosUsuario^ buscarUsuarioID(int id);
+		DatosUsuario^ buscarUsuarioNombre(String^ nombre);
+		DatosUsuario^ buscarUsuarioCredenciales(String^ nombre, String^ contra);
+		bool borrarUsuarioID(int id);
+		bool borrarUsuarioNombre(String^ nombre);
+		DatosUsuario^ restablecerUsuario(String^ nombre, String^ contranueva, String^ confirmacion);
+		DatosUsuario^ modificarUsuarioID(int id, String^ nombre, String^ contra, String^ cargo);
+		List <DatosUsuario^>^ GetUsuarios();
+
+		void registrarRobot(int id, String^ nombre, String^ zona);
+		Robot^ buscarRobotID(int id);
+		Robot^ buscarRobotNombre(String^ nombre);
+		bool borrarRobotID(int id);
+		bool borrarRobotNombre(String^ nombre);
+		Robot^ modificarRobotID(int id, String^ nombre, String^ zona);
+		List <Robot^>^ GetRobots();
 	};
 }
