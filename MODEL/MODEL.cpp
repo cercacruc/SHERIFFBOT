c293ceea1ef5f2082ea5fcc325dcf7c745b293ca
Rodrigo Cercado Lescano <rodrigo.cercado@pucp.edu.pe>
2025-10-07 11:03:36 -0500

    Agregado de capa model

--- MODEL/MODEL.cpp
+++ MODEL/MODEL.cpp
@@ -1,4 +1,78 @@
 #include "pch.h"
-
 #include "MODEL.h"
+
+using namespace System;
+using namespace BotModel;
+
+DatosUsuario::DatosUsuario() {
+	ID = 0;
+	Nombre = nullptr;
+	Contra = nullptr;
+	Cargo = nullptr;
+}
+
+DatosUsuario::DatosUsuario(int id, String^ nombre, String^ contra, String^ cargo) {
+	this->ID = id;
+	this->Nombre = nombre;
+	this->Contra = contra;
+	this->Cargo = cargo;
+}
+int DatosUsuario::getID() {
+	return this->ID;
+}
+void DatosUsuario::setID(int nuevoID) {
+	this->ID = nuevoID;
+}
+
+String^ DatosUsuario::getNombre() {
+	return this->Nombre;
+}
+void DatosUsuario::setNombre(String^ nuevoNombre) {
+	this->Nombre = nuevoNombre;
+}
+
+String^ DatosUsuario::getContrasena() {
+	return this->Contra;
+}
+void DatosUsuario::setContrasena(String^ nuevaContraseña) {
+	this->Contra = nuevaContraseña;
+}
+
+String^ DatosUsuario::getCargo() {
+	return this->Cargo;
+}
+void DatosUsuario::setCargo(String^ nuevoCargo) {
+	this->Cargo = nuevoCargo;
+}
+
+
+Robot::Robot() {
+	ID = 0;
+	Nombre = nullptr;
+	Zona = nullptr;
+}
+Robot::Robot(int ID, String^ nombre, String^ zona) {
+	this->ID = ID;
+	this->Nombre = nombre;
+	this->Zona = zona;
+}
+int Robot::getID() {
+	return this->ID;
+}
+void Robot::setID(int nuevoID) {
+	this->ID = nuevoID;
+}
+String^ Robot::getNombre() {
+	return this->Nombre;
+}
+void Robot::setNombre(String^ nuevoNombre) {
+	this->Nombre = nuevoNombre;
+
+}
+String^ Robot::getZona() {
+	return this->Zona;
+}
+void Robot::setZona(String^ nuevaZona) {
+	this->Zona = nuevaZona;
+}
 
--- MODEL/MODEL.h
+++ MODEL/MODEL.h
@@ -1,10 +1,39 @@
 #pragma once
 
-using namespace System;
+namespace BotModel {
+	using namespace System;
+	public ref class DatosUsuario {
+	private:
+		String^ Nombre;
+		String^ Contra;//Contraseña
+		String^ Cargo;
+		int ID;
+	public:
+		DatosUsuario();
+		DatosUsuario(int ID, String^ nombre, String^ contra, String^ cargo);
+		int getID();
+		void setID(int nuevoID);
+		String^ getNombre();
+		void setNombre(String^ nuevoNombre);
+		String^ getContrasena();
+		void setContrasena(String^ nuevaContraseña);
+		String^ getCargo();
+		void setCargo(String^ nuevoCargo);
+	};
 
-namespace MODEL {
-	public ref class Class1
-	{
-		// TODO: Agregue aquí los métodos de esta clase.
+	public ref class Robot {
+	private:
+		int ID;
+		String^ Nombre;
+		String^ Zona;
+	public:
+		Robot();
+		Robot(int ID, String^ nombre, String^ zona);
+		int getID();
+		void setID(int nuevoID);
+		String^ getNombre();
+		void setNombre(String^ nuevoNombre);
+		String^ getZona();
+		void setZona(String^ nuevaZona);
 	};
 }
