80644767839b39ef85aef2444ad6fa0ac711e4bb
marce <marce@MARCE>
2025-11-11 11:06:48 -0500

    Funciona la camara, pero somos pitufos como Rikolas, ahora solo falta el SQL y la teleoperación

--- SheriffBotGUIApp/ControlRobotForm.h
+++ SheriffBotGUIApp/ControlRobotForm.h
@@ -1,4 +1,10 @@
 #pragma once
+#define CV_DISABLE_OPTIMIZATION
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <msclr/marshal_cppstd.h>
 
 namespace SheriffBotGUIApp {
 
@@ -8,6 +14,7 @@ namespace SheriffBotGUIApp {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace msclr::interop;
 
 	using namespace BotModel;
 	using namespace BotPersistance;
@@ -27,6 +34,9 @@ namespace SheriffBotGUIApp {
 			//TODO: agregar código de constructor aquí
 			//
 			robotEncontrado = robot;
+			timer = gcnew System::Windows::Forms::Timer();
+			timer->Interval = 33; // ~30 fps
+			timer->Tick += gcnew System::EventHandler(this, &ControlRobotForm::timer_Tick);
 		}
 
 	protected:
@@ -53,22 +63,27 @@ namespace SheriffBotGUIApp {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::PictureBox^ pictureBox2;
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::TextBox^ txtRobot;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 
 
 	private:
 		/// <summary>
 		/// Variable del diseñador necesaria.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
+		cv::VideoCapture* cap;
+	private:
+		System::Windows::Forms::Timer^ timer;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Método necesario para admitir el Diseñador. No se puede modificar
 		/// el contenido de este método con el editor de código.
 		/// </summary>
+		
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(ControlRobotForm::typeid));
@@ -81,9 +96,9 @@ namespace SheriffBotGUIApp {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->txtRobot = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->giroIzquierda))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->giroDerecha))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox5))->BeginInit();
@@ -190,16 +205,6 @@ namespace SheriffBotGUIApp {
 			this->label2->TabIndex = 13;
 			this->label2->Text = L"Posición del robot:";
 			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(16, 122);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(294, 198);
-			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
-			this->pictureBox1->TabIndex = 12;
-			this->pictureBox1->TabStop = false;
-			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
@@ -218,11 +223,21 @@ namespace SheriffBotGUIApp {
 			this->txtRobot->Size = System::Drawing::Size(138, 29);
 			this->txtRobot->TabIndex = 22;
 			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(16, 122);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(302, 197);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 23;
+			this->pictureBox1->TabStop = false;
+			// 
 			// ControlRobotForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(741, 639);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->txtRobot);
 			this->Controls->Add(this->giroIzquierda);
 			this->Controls->Add(this->giroDerecha);
@@ -233,7 +248,6 @@ namespace SheriffBotGUIApp {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->pictureBox2);
 			this->Controls->Add(this->label2);
-			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label1);
 			this->Name = L"ControlRobotForm";
 			this->Text = L"ControlRobotForm";
@@ -272,6 +286,49 @@ namespace SheriffBotGUIApp {
 		private: System::Void ControlRobotForm_Load(System::Object^ sender, System::EventArgs^ e) {
 			if (robotEncontrado != nullptr) {
 				txtRobot->Text = robotEncontrado->Nombre;
+				cap = new cv::VideoCapture(0, cv::CAP_DSHOW);
+				if (!cap->isOpened()) {
+					MessageBox::Show("No se pudo abrir la cámara");
+					return;
+				}
+				timer->Start();
+			}
+		}
+			   void timer_Tick(System::Object^ sender, System::EventArgs^ e)
+			   {
+				   cv::Mat frame;
+				   if (!cap->read(frame) || frame.empty()) return;
+
+				   cv::cvtColor(frame, frame, cv::COLOR_RGB2BGR);
+
+				   cv::Mat* clonedFrame = new cv::Mat(frame.clone());
+
+				   System::Drawing::Bitmap^ bmp = gcnew System::Drawing::Bitmap(
+					   clonedFrame->cols, clonedFrame->rows, clonedFrame->step,
+					   System::Drawing::Imaging::PixelFormat::Format24bppRgb,
+					   System::IntPtr(clonedFrame->data));
+
+				   bmp->Tag = System::IntPtr(clonedFrame);
+
+				   if (pictureBox1->Image != nullptr)
+				   {
+					   Bitmap_Disposed(pictureBox1->Image, nullptr);
+					   delete pictureBox1->Image;
+				   }
+
+				   pictureBox1->Image = bmp;
+			   }
+		private: void Bitmap_Disposed(System::Object^ sender, System::EventArgs^ e) {
+			System::Drawing::Bitmap^ bmp = safe_cast<System::Drawing::Bitmap^>(sender);
+
+			if (bmp->Tag != nullptr)
+			{
+				System::IntPtr ptr = safe_cast<System::IntPtr>(bmp->Tag);
+				cv::Mat* matPtr = static_cast<cv::Mat*>(ptr.ToPointer());
+				if (matPtr != nullptr) {
+					delete matPtr;
+					bmp->Tag = nullptr;
+				}
 			}
 		}
 	};
