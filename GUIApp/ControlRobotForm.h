362f6f1ab5b4ab11d70ad76c17d1749a7e6fd746
Rodrigo Cercado Lescano <rodrigo.cercado@pucp.edu.pe>
2025-12-15 18:39:46 -0500

    descomentado del mqtt

--- GUIApp/ControlRobotForm.h
+++ GUIApp/ControlRobotForm.h
@@ -414,10 +414,10 @@ namespace GUIApp {
 		}
 
 		// Conectar con MQTT al carga
-		/*if (!Vehiculo::Connect()) {
+		if (!Vehiculo::Connect()) {
 			MessageBox::Show("No se pudo conectar con MQTT", "Advertencia",
 				MessageBoxButtons::OK, MessageBoxIcon::Warning);
-		}*/
+		}
 		
 		try {
 			if (!this->InicializarCamara()) {
